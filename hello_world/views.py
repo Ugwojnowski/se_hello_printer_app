007203baab9c5b51506731fdbb748ecb51da7319
Ugwojnowski <Ugwojnowski@users.noreply.github.com>
2020-05-06 17:03:44 +0200

    Pewne zmiany w kodzie

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "AlicjaWcislo"
+moje_imie = "UrbanWojnowski"
 msg = "Hello World!"
 
 
@@ -12,8 +12,7 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie, output.lower())
 
 
 @app.route('/outputs')
